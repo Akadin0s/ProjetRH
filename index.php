1a4614d39f3fc5518adfb775d36be9ae9bb2d344
Adnane Ouadih <ouadihadnane15@gmail.com>
2025-02-03 22:46:23 +0100

    index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!-- Navbar content -->
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand">Ressource Humaine</a>
+                <div style="margin-right: -330px;">
+                    <a href="info.php" class="navbar-brand1"> <i></i>Ajoute de donnee</a>
+                    <a href="conge.php" class="navbar-brand1"> <i></i>Conge</a>
+                    <a href="searchpage.php" class="navbar-brand1"><i></i>Tableau</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
